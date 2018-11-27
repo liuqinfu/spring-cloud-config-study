6b17bfc2cdb424568e936429f8f268d49347deea
我走路带风 <liuqinfu@wozouludaifengdeMacBook-Pro.local>
2018-11-27 14:47:58 +0800

    Initial commit

---
+++ src/main/java/com/study/springcloudconfigstudy/SpringCloudConfigStudyApplication.java
@@ -0,0 +1,12 @@
+package com.study.springcloudconfigstudy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringCloudConfigStudyApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SpringCloudConfigStudyApplication.class, args);
+    }
+}
